fbd38180ae52eeead472f2324d79908e0ed1d728
modelpath-dev <cml.codes@gmail.com>
2025-01-27 13:04:00 +0530

    v4

---
+++ app.py
@@ -0,0 +1,12 @@
+import close
+
+
+from close import create_brochure
+if __name__ == "__main__":
+    company_name = "HuggingFace"
+    url = "https://lora12434334.co/"
+
+    brochure = create_brochure(company_name, url)
+    print(brochure)
+    
+
